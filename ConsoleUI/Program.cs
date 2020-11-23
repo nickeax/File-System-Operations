f4183e1a27a4f19aa7b8ae815ba406bb5a50df11
Nick Fletcher <nickeax@gmail.com>
2020-11-23 14:19:54 +1100

    Tried various operations.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ConsoleUI
 {
@@ -6,7 +9,15 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootPath = @"C:\Users\nicke\OneDrive\dev\eastern-press\exam-results\support\";
+
+            var files = Directory.GetFiles(rootPath, "*.*", SearchOption.TopDirectoryOnly);
+
+            foreach (var file in files)
+            {
+                var fi = new FileInfo(Path.Combine(rootPath, file));
+                Console.WriteLine(fi.Length);
+            }
         }
     }
 }
